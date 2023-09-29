ab504627965fb9a555be71cfdb596f533f81c828
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-29 05:24:05 -0700

    rsa

--- rsa.c
+++ rsa.c
@@ -1,77 +1,91 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
+#include <time.h>
 /**
- * is_prime - to check if number is prime
- * @num: number is prie or not
- * Return: false or True
+ * gcd - greatest common divisor
+ * @a: divisible num a
+ * @b: divisible num b
+ * Return: return success or failure.
  */
-bool is_prime(long long num)
+uint64_t gcd(uint64_t a, uint64_t b)
 {
-	if (num <= 1) return false;
-	if (num <= 3) return true;
-	if (num % 2 == 0 || num % 3 == 0) return false;
-
-	for (long long i = 5; i * i <= num; i += 6)
+	if (b == 0)
 	{
-		if (num % i == 0 || num % (i + 2) == 0)
-		{
-			return (false);
-		}
+		return (a);
 	}
-	return (true);
+	else
+	{
+		return gcd(b, a % b);
+	}
 }
-
 /**
- * factorize_rsa - factorize number into prime p and q.
- * @n: number n into prime factors
- * Return: 0 or 1.
+ * pollards_rho - algorithm for integer factorization
+ * @n: number to factorize
+ * Return: return success or failure
  */
-void factorize_rsa(long long n)
+uint64_t pollards_rho(uint64_t n)
 {
-	for (long long p = 2; p <= n; p++)
+	uint64_t x = 2, y = 2, d = 1;
+	while (d == 1)
 	{
-		if (n % p == 0 && is_prime(p))
-		{
-			long long q = n / p;
-			if (is_prime(q))
-			{
-				printf("%lld=%lld*%lld\n", n, p, q);
-				return;
-			}
-		}
+		x = (x * x + 1) % n;
+		y = (y * y + 1) % n;
+		y = (y * y + 1) % n;
+		d = gcd(abs(x - y), n);
 	}
-	printf("No prime factorization found for %lld\n", n);
+	return (d);
 }
-
 /**
- * main - main function
- * @argc: argument count
+ * main - main argument
  * @argv: argument vector
- * Return: 0 or 1.
+ * @argc: argument count
+ * Return: return success or failure
  */
 int main(int argc, char *argv[])
 {
-	FILE *file = fopen(argv[1], "r");
-	long long num;
-	fclose(file);
-
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return(1);
+		printf("Usage: %s <file>\n", argv[0]);
+		return (1);
 	}
 
+	FILE *file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		perror("Error opening file");
 		return (1);
 	}
 
-	for (int i = 0; fscanf(file, "%lld\n", &num) == 1; i++)
+	uint64_t num;
+	if (fscanf(file, "%lu", &num) != 1)
 	{
-		factorize_rsa(num);
+
+		printf("Error reading the number from the file\n");
+		fclose(file);
+		return (1);
 	}
-		return (0);
+
+	fclose(file);
+
+	/* start the timer */
+	clock_t start_time = clock();
+	/* factorize the number pollard */
+	uint64_t factor = pollards_rho(num);
+	/*stop the timer */
+	clock_t end_time = clock();
+
+	if (factor == 1 || factor == num)
+	{
+		printf("%lu=%lu*%u\n", num, num, 1);
+	}
+	else
+	{
+		printf("%lu=%lu*%lu\n", num, factor, num / factor);
+	}
+
+	double execution_time = (double)(end_time = start_time) / CLOCKS_PER_SEC;
+	printf("Execution Time: %.6f seconds\n", execution_time);
+
+	return (0);
 }
